6224033acfb3a506b2e7dbff284f3310ef0bf729
Bahram Mohmand <bahram.mohmand123@gmail.com>
2025-11-03 17:27:59 -0800

    setup constants

--- lab_7.py
+++ lab_7.py
@@ -15,10 +15,10 @@ sys.path.append(os.path.dirname(__file__))
 IMAGE_WIDTH = 700
 
 # TODO: Define constants for the state machine behavior
-TIMEOUT = pass  # TODO: Set the timeout threshold (in seconds) for determining when a detection is too old
-SEARCH_YAW_VEL = pass  # TODO: Set the angular velocity (rad/s) for rotating while searching for the target
-TRACK_FORWARD_VEL = pass  # TODO: Set the forward velocity (m/s) while tracking the target
-KP = pass  # TODO: Set the proportional gain for the proportional controller that centers the target
+TIMEOUT = 5  # TODO: Set the timeout threshold (in seconds) for determining when a detection is too old
+SEARCH_YAW_VEL = 0.5  # TODO: Set the angular velocity (rad/s) for rotating while searching for the target
+TRACK_FORWARD_VEL = 0.1  # TODO: Set the forward velocity (m/s) while tracking the target
+KP = 0.7  # TODO: Set the proportional gain for the proportional controller that centers the target
 
 class State(Enum):
     IDLE = 0     # Stay in place, no tracking
